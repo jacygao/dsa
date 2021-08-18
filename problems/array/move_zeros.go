d302d48bba2b0b0d92a1b7967a2631a80687beb6
jacygao <aus.jacy@gmail.com>
2021-08-18 23:58:31 +1000

    added comments and solution 2

--- problems/array/move_zeros.go
+++ problems/array/move_zeros.go
@@ -1,6 +1,23 @@
 package array
 
-func moveZeroes(nums []int) {
+/* MoveZeroes solves Leetcode 283:
+
+Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+
+Note that you must do this in-place without making a copy of the array.
+
+Example 1:
+
+Input: nums = [0,1,0,3,12]
+Output: [1,3,12,0,0]
+
+Example 2:
+
+Input: nums = [0]
+Output: [0]
+
+*/
+func MoveZeroesSol1(nums []int) {
 	l, r := 0, 1
 	for r < len(nums) {
 		if nums[l] != 0 {
@@ -15,5 +32,18 @@ func moveZeroes(nums []int) {
 			continue
 		}
 		r++
+	}
+}
+
+func MoveZeroesSol2(nums []int) {
+	for i := 0; i < len(nums)-1; i++ {
+		if nums[i] == 0 {
+			for j := i + 1; j < len(nums); j++ {
+				if nums[j] != 0 {
+					nums[i], nums[j] = nums[j], nums[i]
+					break
+				}
+			}
+		}
 	}
 }
