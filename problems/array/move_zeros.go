151c3dfd2cf98279698f0e61f116ec7c01aa2a21
jacygao <aus.jacy@gmail.com>
2021-08-19 00:10:18 +1000

    added simplified code approach with benchmark

--- problems/array/move_zeros.go
+++ problems/array/move_zeros.go
@@ -47,3 +47,14 @@ func MoveZeroesSol2(nums []int) {
 		}
 	}
 }
+
+func MoveZeroesSol3(nums []int) {
+	slow, fast := 0, 0
+	for fast < len(nums) {
+		if nums[fast] != 0 {
+			nums[slow], nums[fast] = nums[fast], nums[slow]
+			slow++
+		}
+		fast++
+	}
+}
