dc84695c946d3daa9725f42e7d6633b24f8fba69
jgao <aus.jacy@gmail.com>
2021-09-19 20:51:36 +1000

    added kahn's algorithm implementation for topological sort

--- graph/topological_sort.go
+++ graph/topological_sort.go
@@ -2,7 +2,8 @@ package graph
 
 import "container/list"
 
-func TopologicalSort(n int, edges [][]int) []int {
+// TopologicalSortDFS implements the Topological Sorting algorithm using the DFS approach.
+func TopologicalSortDFS(n int, edges [][]int) []int {
 	ans := []int{}
 	g := genDirectedAdj(n, edges)
 	stack := list.New()
@@ -31,6 +32,39 @@ func dfsTopologicalSort(g [][]int, n int, s *list.List, visited []int) {
 	}
 	s.PushBack(n)
 }
+
+// TopologicalSortKahn implements the Topological Sorting algorithm using Kahn's algorithm.
+func TopologicalSortKahn(n int, edges [][]int) []int {
+	graph := genDirectedAdj(n, edges)
+	queue := []int{}
+	degrees := make([]int, n)
+	res := []int{}
+
+	for _, edge := range edges {
+		degrees[edge[1]]++ 
+	}
+
+	for node, degree := range degrees {
+		if degree == 0 {
+			queue = append(queue, node)
+		}
+	}
+
+	for len(queue) > 0 {
+		peek := queue[0]
+		queue = queue[1:]
+		res = append(res, peek)
+		for _, node := range graph[peek] {
+			degrees[node]--
+			if degrees[node] == 0 {
+				queue = append(queue, node)
+			}
+		}
+	}
+
+	return res
+}
+
 
 func genDirectedAdj(n int, edges [][]int) [][]int {
 	g := make([][]int, n)
--- graph/topological_sort_test.go
+++ graph/topological_sort_test.go
@@ -5,7 +5,7 @@ import (
 	"testing"
 )
 
-func TestTopologicalSort(t *testing.T) {
+func TestTopologicalSortDFS(t *testing.T) {
 	n := 6
 	edges := [][]int{
 		{0, 2},
@@ -18,7 +18,27 @@ func TestTopologicalSort(t *testing.T) {
 	}
 
 	expected := []int{5, 4, 0, 3, 1, 2}
-	res := TopologicalSort(n, edges)
+	res := TopologicalSortDFS(n, edges)
+
+	if !reflect.DeepEqual(res, expected) {
+		t.Fatalf("results do not match! expcted %+v but got %+v", expected, res)
+	}
+}
+
+func TestTopologicalSortKahn(t *testing.T) {
+	n := 6
+	edges := [][]int{
+		{0, 2},
+		{0, 3},
+		{3, 1},
+		{4, 1},
+		{4, 2},
+		{5, 0},
+		{5, 2},
+	}
+
+	expected := []int{4, 5, 0, 2, 3, 1}
+	res := TopologicalSortKahn(n, edges)
 
 	if !reflect.DeepEqual(res, expected) {
 		t.Fatalf("results do not match! expcted %+v but got %+v", expected, res)
